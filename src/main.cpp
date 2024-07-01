9a38d2f7bf91cbf4b1f3f83c80ae199f445623ae
DanieLTjach <gamemans.vkgm@gmail.com>
2024-07-01 21:59:57 +0300

    Add main led effects + web interface

--- src/main.cpp
+++ src/main.cpp
@@ -3,52 +3,259 @@
 #include <FastLED.h>
 #include "WiFi.h"
 
-
 // Global variables
-#define LED_PIN 26;
-#define LED_COUNT 180;
+#define LED_PIN 26
+#define LED_COUNT 180
 #define AP_SSID "Alien"
 #define AP_PASS "bezparolianehodi"
+
+// FastLED
+struct CRGB leds[LED_COUNT];
+
+int eff_index;
+bool power = true;
+int BRIGHT;
+float SPEED = 20;
+int STEP = 20;
+int ihue = 0;
+int thissat = 255;
+unsigned long lastmillis = 0;
+GPcolor rgb(0, 0, 0);
+
+int currentEffect = 0;
 
 GyverPortal ui;
 
 // Список эффектов
-char* effects[5] = {
-  "Rainbow",
-  "Confetti",
-  "Sinelon",
-  "Juggle",
-  ""
-};
+String effects[6] = {
+    "Static",
+    "Rainbow LOOP",
+    "Rainbow Fade",
+    "Rainbow Cycle",
+    "Music",
+    ""};
 
-// конструктор страницы
-void build(GyverPortal& p) {
-  GP.BUILD_BEGIN(GP_DARK);
-  GP.PAGE_TITLE("ESPLight"); 
-  
-  GP.TITLE("ESPLight"); GP.BREAK();
-  GP.SPAN("Effect:"); GP.BREAK();
-  GP.SELECT("Eff_SEL", effects); GP.BREAK();
-  GP.BUTTON("clk", "Click");  GP.BREAK();
-  GP.BUTTON("ext", "Exit");   GP.BREAK();
-  //GP.FILE_UPLOAD("test.txt"); Требует предварительного монтирования файловой системы
-  GP.BUILD_END();
+void oneColorAll()
+{ //-SET ALL LEDS TO ONE COLOR
+  for (int i = 0; i < LED_COUNT; i++)
+  {
+    leds[i].setRGB(rgb.r, rgb.g, rgb.b);
+  }
+  FastLED.setBrightness(BRIGHT);
+  FastLED.show();
 }
 
-//Обработчик событий
-void action(GyverPortal& p) {
-  if (p.click("clk")) Serial.println("Button click");
-  if (p.click("ext")) {
-    Serial.println("Exit portal");
-    p.stop();
+void clearLED()
+{
+  for (int i = 0; i < LED_COUNT; i++)
+  {
+    leds[i].setRGB(0, 0, 0);
+  }
+  FastLED.setBrightness(0);
+  FastLED.show();
+}
+
+void rainbowFade()
+{ //-m2-FADE ALL LEDS THROUGH HSV RAINBOW
+  if (lastmillis + SPEED < millis() && power)
+  {
+    ihue++;
+    if (ihue > 255)
+    {
+      ihue = 0;
+    }
+    for (int idex = 0; idex < LED_COUNT; idex++)
+    {
+      leds[idex] = CHSV(ihue, thissat, 255);
+    }
+    LEDS.setBrightness(BRIGHT);
+    LEDS.show();
+    lastmillis = millis();
   }
 }
 
-void setup() {
+void rainbowLoop()
+{ //-m3-LOOP HSV RAINBOW
+  if (lastmillis + SPEED < millis() && power)
+  {
+    for(int idex = 0; idex < LED_COUNT; idex++){
+      ihue = ihue + STEP;
+
+      if (ihue > 255)
+      {
+        ihue = 0;
+      }
+        leds[idex] = CHSV(ihue, thissat, 255);
+
+      FastLED.setBrightness(BRIGHT);
+      FastLED.show();
+      lastmillis = millis();
+    }
+  }
+}
+
+void rainbowCycle(){
+  byte r, g, b;
+  if(lastmillis + 500 < millis() && power){
+    for(uint16_t i = 0; i < 256 * 2; i++){
+      for(uint16_t j = 0; j < LED_COUNT; j++){
+        byte WheelPos = ((j * 256 / LED_COUNT) + i) & 255;
+        if(WheelPos < 85){
+          r = WheelPos * 3;
+          g = 255 - WheelPos * 3;
+          b = 0;
+        }else if(WheelPos < 170){
+          r = 255 - WheelPos * 3;
+          g = 0;
+          b = WheelPos * 3;
+        }else{
+          r = 0;
+          g = WheelPos * 3;
+          b = 255 - WheelPos * 3;
+        }
+        leds[j].setRGB(r, g, b);
+      }
+       FastLED.show();
+    }
+    FastLED.setBrightness(BRIGHT);
+    lastmillis = millis();
+  }
+}
+
+// конструктор страницы
+void build(GyverPortal &p)
+{
+  GP.BUILD_BEGIN(GP_DARK);
+  GP.PAGE_TITLE("ESPLight");
+
+  GP.TITLE("ESPLight");
+  GP.BREAK();
+
+  GP.BLOCK_BEGIN("Effect");
+  GP.SPAN("Effect:");
+  GP.SELECT("Eff_SEL", effects, eff_index, true, 0, 0);
+  GP.RELOAD_CLICK("Eff_SEL");
+  GP.BLOCK_END();
+  GP.BREAK();
+
+  GP.BLOCK_BEGIN("Settings");
+  GP.TITLE("Settings");
+  GP.SPAN(effects[eff_index]);
+  GP.BREAK();
+  switch (eff_index)
+  {
+  case 0:
+    GP.BOX_BEGIN();
+    GP.SPAN("POWER:");
+    GP.SWITCH("POWER", power);
+    GP.BOX_END();
+    GP.BOX_BEGIN();
+    GP.SPAN("RGB:");
+    GP.COLOR("RGB", rgb);
+    GP.BOX_END();
+    GP.BOX_BEGIN();
+    GP.SPAN("Brightness:");
+    GP.SLIDER("BRIGHT", BRIGHT, 0, 255);
+    GP.BOX_END();
+    break;
+  case 1:
+    GP.BOX_BEGIN();
+    GP.SPAN("POWER:");
+    GP.SWITCH("POWER", power);
+    GP.BOX_END();
+    GP.BOX_BEGIN();
+    GP.SPAN("Brightness:");
+    GP.SLIDER("BRIGHT", BRIGHT, 0, 255);
+    GP.BOX_END();
+    GP.BOX_BEGIN();
+    GP.SPAN("Speed:");
+    GP.SLIDER("SPEED", SPEED, 0, 100, 2);
+    GP.BOX_END();
+    GP.BOX_BEGIN();
+    GP.SPAN("Step:");
+    GP.SLIDER("STEP", STEP, 0, LED_COUNT);
+    GP.BOX_END();
+
+    break;
+  case 2:
+    GP.BOX_BEGIN();
+    GP.SPAN("POWER:");
+    GP.SWITCH("POWER", power);
+    GP.BOX_END();
+    GP.BOX_BEGIN();
+    GP.SPAN("Brightness:");
+    GP.SLIDER("BRIGHT", BRIGHT, 0, 255);
+    GP.BOX_END();
+    GP.BOX_BEGIN();
+    GP.SPAN("Speed:");
+    GP.SLIDER("SPEED", SPEED, 0, 100, 2);
+    GP.BOX_END();
+    break;
+  case 3:
+    GP.BOX_BEGIN();
+    GP.SPAN("POWER:");
+    GP.SWITCH("POWER", power);
+    GP.BOX_END();
+    GP.BOX_BEGIN();
+    GP.SPAN("Brightness:");
+    GP.SLIDER("BRIGHT", BRIGHT, 0, 255);
+    GP.BOX_END();
+    GP.BOX_BEGIN();
+    GP.SPAN("Speed:");
+    GP.SLIDER("SPEED", SPEED, 0, 100, 2);
+    GP.BOX_END();
+    break;
+  case 4:
+    GP.BOX_BEGIN();
+    GP.SPAN("Not Available");
+    GP.BOX_END();
+    break;
+  default:
+    break;
+  }
+
+  GP.BLOCK_END();
+  GP.BREAK();
+  GP.BUILD_END();
+}
+
+// Обработчик событий
+void action(GyverPortal &p)
+{
+  if (p.click())
+  {
+    p.clickInt("Eff_SEL", eff_index);
+    p.clickBool("POWER", power);
+    p.clickColor("RGB", rgb);
+    p.clickInt("BRIGHT", BRIGHT);
+    p.clickFloat("SPEED", SPEED);
+    if(!power){
+      currentEffect = -1;
+      clearLED();
+    }else{
+      currentEffect = eff_index;
+    }
+  }
+  if (p.update())
+  {
+  }
+}
+
+void setup()
+{
   Serial.begin(9600);
+  IPAddress ip(192, 168, 31, 239);
+  IPAddress gateway(192, 168, 31, 1);
+  IPAddress subnet(255, 255, 255, 0);
+
+  FastLED.addLeds<WS2811, LED_PIN, GRB>(leds, LED_COUNT).setCorrection(TypicalLEDStrip);
+
+
   WiFi.mode(WIFI_STA);
   WiFi.begin(AP_SSID, AP_PASS);
-  while (WiFi.status() != WL_CONNECTED) {
+  WiFi.config(ip, gateway, subnet);
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
@@ -59,11 +266,34 @@ void setup() {
   ui.attach(action);
   ui.start();
   Serial.println("Portal run");
-  while (ui.tick());
+  while (ui.tick())
+  {
+    switch (currentEffect)
+      {
+      case 0:
+        oneColorAll();
+        break;
+      case 1:
+        rainbowLoop();
+        break;
+      case 2:
+        rainbowFade();
+        break;
+      case 3:
+        rainbowCycle();
+      break;
+      case 4:
+      
+      break;
+      default:
+        
+        break;
+      }
+  }
 
   Serial.println("Setup end");
 }
 
-
-void loop() {
+void loop()
+{
 }
